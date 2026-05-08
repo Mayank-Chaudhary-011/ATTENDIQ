8d7d5b3333c31e99a2bf05182a32ddd897362528
MAXXX <maxxxxx0711@gmail.com>
2026-05-08 10:58:55 +0530

    Added attendance dialogs and fixed enrollment flow

--- app.py
+++ app.py
@@ -3,8 +3,13 @@ import streamlit as st
 from src.screen.home_screen import home_screen
 from src.screen.teacher_screen import teacher_screen
 from src.screen.student_screen import student_screen
+from src.components.dialog_auto_enroll import auto_enroll_dialog
 
 def main():
+    st.set_page_config(
+        page_title='AttendIQ - Smart AI-Powered Attendance Management System',
+        page_icon="https://i.ibb.co/LDpKN5xc/Copilot-20260508-102938.png"
+    )
     if 'login_type' not in st.session_state:
         st.session_state['login_type'] = None
     
@@ -13,6 +18,14 @@ def main():
             teacher_screen()
         case 'student':
             student_screen()
-        case None:
+        case None: 
             home_screen()
+
+    join_code = st.query_params.get('join-code')
+    if join_code:
+        if st.session_state.login_type != 'student':
+            st.session_state.login_type = 'student'
+            st.rerun()
+        if st.session_state.get('is_logged_in') and st.session_state.get('user_role') == 'student':
+            auto_enroll_dialog(join_code)
 main()
